6511969c3d146cbbae72bbe4b7333c300d34037a
Tyler <traebirdsong@gmail.com>
2023-03-07 15:30:15 -0600

    Add playRound function

--- scripts.js
+++ scripts.js
@@ -1,11 +1,19 @@
 const choices = ['rock','paper','scissors'];
 
 function getComputerChoice() {
+  // chooses a random item from choices
   return choices[Math.floor(Math.random()*choices.length)];
 }
 
 function getPlayerChoice() {
+  // prompts user for an their choice
+  // lowercases their input
   let playerChoice = prompt("Rock, paper, or scissors? ");
   return playerChoice.toLowerCase();
 }
 
+function playRound() {
+  // plays a single round
+  const computerSelection = getComputerChoice();
+  const playerSelection = getPlayerChoice();
+}
