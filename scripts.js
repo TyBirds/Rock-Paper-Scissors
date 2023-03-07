89e64ff17210a9dd21dc12c9080999686af2f6f3
Tyler <traebirdsong@gmail.com>
2023-03-07 15:02:43 -0600

    Add getPlayerChoice function

--- scripts.js
+++ scripts.js
@@ -4,3 +4,7 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random()*choices.length)];
 }
 
+function getPlayerChoice() {
+  const playerChoice = prompt("Rock, paper, or scissors? ");
+  return playerChoice;
+}
